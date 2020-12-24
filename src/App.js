b5e4955155be59596fe538658f783e70f395b33b
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-23 20:27:52 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import { Container } from "@material-ui/core";
+import React from "react";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const city = e.target.city.value;
+    console.log(city);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="lg" style={{ backgroundColor: "pink" }}>
+        <h4>Weather</h4>
+        <form onSubmit={submitHandler}>
+          <TextField
+            id="city"
+            label="Type city"
+            variant="outlined"
+            name="city"
+          />
+          <Button type="submit" color="primary">
+            send!
+          </Button>
+        </form>
+      </Container>
     </div>
   );
 }
